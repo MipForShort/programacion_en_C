6b55596c3f11bd363d5cf3409c139ff9865e8e76
Mario Mip <mariomip98@gmail.com>
2025-01-09 18:53:09 -0600

    First commit

---
+++ 2. El Primer Programa en C/hola_mundo.c
@@ -0,0 +1,17 @@
+/*******************************************************
+ *
+ * FICHERO: hola_mundo.c
+ *
+ * CONTENIDO: Este un programa que escribe Hola, mundo
+ *
+ *******************************************************/
+
+/* Include */
+#include <stdio.h>
+
+/* Creacion de la funcion main, puede dar advertencia el tipo void */
+void main()
+{
+    /* Esta linea imprime el texto con formato: Hola, mundo */
+    printf("Hola, mundo\n");
+}
