94b45b4811d89b0ffe1ee9679231cc07b0b2ca40
Mario Mip <mariomip98@gmail.com>
2025-01-09 19:55:44 -0600

    Added the file variables.c

---
+++ 3. Variables en C/variables.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define PI 3.14
+
+const int x = 15;
+
+
+int main(void)
+{
+    int i;
+    char letra;
+    double r;
+    long int j;
+
+    i = 0;
+
+    printf("Ingresa un numero:\n");
+    scanf("%d", &i);
+    printf("%d\n", i);
+
+}
